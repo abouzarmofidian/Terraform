1b694b3990c5822e42dd4afb50b49041ad2d371f
a.mofidian <a.mofidian@amofidians-MacBook-Air.local>
2023-03-03 12:40:35 +0330

    create own vpc&subnet

--- main.tf
+++ main.tf
@@ -1,65 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.13.4"
-}
-
-provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    apigatewayv2   = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    elasticache    = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    rds            = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    s3             = "http://s3.localhost.localstack.cloud:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
-  }
-}
-
+#VPC
 resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-}
-
-
-resource "aws_instance" "myserver" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
+  cidr_block = var.vpc_cidr
   tags = {
-    Name = "Server",
+    Name = var.vpc_name
   }
 }
 
-output "IP" {
-  value = aws_instance.myserver.public_ip
+#Subnet
+resource "aws_subnet" "my-subnet" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  availability_zone       = var.availability_zone
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = var.subnet_name
+  }
+
 }
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+vpc_name          = "my-vpc"
+vpc_cidr          = "10.10.0.0/16"
+subnet_name       = "my-subnet"
+subnet_cidr       = "10.10.10.0/24"
+availability_zone = "us-east-1a"
